4d7c2a3895bb030de06dccc3b8bbafdbae1c08f7
Stijn Mertens <stijn.mertens@expertum.net>
2021-09-05 20:15:11 +0200

    - Bugfixes structure manager - Trying to change harvester so it also brings energy to extensions

--- src/manager.structures.js
+++ src/manager.structures.js
@@ -8,11 +8,11 @@ var structManager = {
         // Check if there are already extenions build
         const extensions = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_EXTENSION }});
         console.log('Spawn has ' + extensions.length + ' extensions available');
-        const constructionSites = room.find(FIND_MY_STRUCTURES, { filter: { structureType: FIND_MY_CONSTRUCTION_SITES }});
+        const constructionSites = room.find(FIND_MY_CONSTRUCTION_SITES);
         console.log('Room has ' + constructionSites.length + ' construction sites');
         // TODO: fix this, until extensions are fully build, this check is 0
         // => Store in room memory
-        if (extensions.length == 0 && constructionSites < 5) {
+        if (extensions.length == 0 && constructionSites.length < 5) {
           // TODO: check if the given locations around our spawn are free of other structures
           // Build extensions around our spawn
           room.createConstructionSite(sSpawnPoint.pos.x, sSpawnPoint.pos.y-1, STRUCTURE_EXTENSION); // top
@@ -22,6 +22,13 @@ var structManager = {
           // TODO: build number 5
           // Increase number of extensions in our room memory
         }
+/*
+        // Test codes
+        for (var site in constructionSites) {
+          console.log(site);
+          console.log(site.id + " has stucture type" + site.structureType);
+        }
+*/
       }
     }
 };
--- src/role.harvester.js
+++ src/role.harvester.js
@@ -11,20 +11,37 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.carry.energy < creep.carryCapacity) {
-            // TODO: sources already searched in main
-            // - loop found found sources
-            // - assign (empty) creep to source with most energy
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-            }
+      if(creep.carry.energy < creep.carryCapacity) {
+          // TODO: sources already searched in main
+          // - loop found found sources
+          // - assign (empty) creep to source with most energy
+          var sources = creep.room.find(FIND_SOURCES);
+          if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+          }
+      }
+      else {
+        // find targets (spawn or extensions) that can receive energy
+        let targets = creep.room.find(FIND_STRUCTURES, { filter: (structure) => {return
+                                        (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN)
+                                        && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;}
+                                      });
+        console.log('Found ' + targets.length + ' in need of energy');
+        // TODO: filter above not working as expected
+        let testtargets = creep.room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_EXTENSION }});
+        console.log('Found ' + testtargets.length + ' testtargets: ' + testtargets);
+
+        let extensions = creep.room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_EXTENSION }});
+        for (let extension in extensions) {
+          console.log('extension free capacity: ' + extension.getFreeCapacity);
         }
-        else if(Game.spawns['MilkyWay'].store[RESOURCE_ENERGY] < Game.spawns['MilkyWay'].store.getCapacity(RESOURCE_ENERGY)) {
-            if(creep.transfer(Game.spawns['MilkyWay'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(Game.spawns['MilkyWay']);
-            }
+
+        if(targets.length > 0) {
+          if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+            creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+          }
         }
+      }
     }
 };
 
