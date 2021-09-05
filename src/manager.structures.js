73fd7ac47f35f7e7bc58c8b385606abebf528e7d
Stijn Mertens <stijn.mertens@expertum.net>
2021-09-05 12:40:51 +0200

    Initial version structure manager

---
+++ src/manager.structures.js
@@ -0,0 +1,29 @@
+var structManager = {
+
+    /** @param {StructureSpawn} sSpawnPoint **/
+    buildExtensions: function(sSpawnPoint) {
+      // We can only build extensions starting at controller level 2
+      let room = sSpawnPoint.room;
+      if (room.controller.level > 1) {
+        // Check if there are already extenions build
+        const extensions = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_EXTENSION }});
+        console.log('Spawn has ' + extensions.length + ' extensions available');
+        const constructionSites = room.find(FIND_MY_STRUCTURES, { filter: { structureType: FIND_MY_CONSTRUCTION_SITES }});
+        console.log('Room has ' + constructionSites.length + ' construction sites');
+        // TODO: fix this, until extensions are fully build, this check is 0
+        // => Store in room memory
+        if (extensions.length == 0 && constructionSites < 5) {
+          // TODO: check if the given locations around our spawn are free of other structures
+          // Build extensions around our spawn
+          room.createConstructionSite(sSpawnPoint.pos.x, sSpawnPoint.pos.y-1, STRUCTURE_EXTENSION); // top
+          room.createConstructionSite(sSpawnPoint.pos.x, sSpawnPoint.pos.y+1, STRUCTURE_EXTENSION); // bottom
+          room.createConstructionSite(sSpawnPoint.pos.x-1, sSpawnPoint.pos.y, STRUCTURE_EXTENSION); // left
+          room.createConstructionSite(sSpawnPoint.pos.x+1, sSpawnPoint.pos.y, STRUCTURE_EXTENSION); // right
+          // TODO: build number 5
+          // Increase number of extensions in our room memory
+        }
+      }
+    }
+};
+
+module.exports = structManager;
