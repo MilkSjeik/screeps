c1ae5259acaa1869c3ce4d0b537d6bf2a17b8465
stijnme <stijn.rm.mertens@gmail.com>
2021-09-06 21:49:02 +0200

    Update role.harvester.js
    harvester now brings energy to spawn and extensions

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -11,39 +11,35 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-      if(creep.carry.energy < creep.carryCapacity) {
-          // TODO: sources already searched in main
-          // - loop found found sources
-          // - assign (empty) creep to source with most energy
-          var sources = creep.room.find(FIND_SOURCES);
-          if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-              creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-          }
-      }
-      else {
-        // find targets (spawn or extensions) that can receive energy
-        let targets = creep.room.find(FIND_STRUCTURES, { filter: (structure) => {return
-                                        (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN)
-                                        && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;}
-                                      });
-        console.log('Found ' + targets.length + ' in need of energy');
-        // TODO: filter above not working as expected
-        let testtargets = creep.room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_EXTENSION }});
-        console.log('Found ' + testtargets.length + ' testtargets: ' + testtargets);
-
-        // TODO: following works:
-        let extensions = creep.room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_EXTENSION }});
-        //console.log('Extensions: ' + extensions);
-        for (let extension in extensions) {
-          console.log('extension ' + extensions[extension].id + ' free capacity: ' + extensions[extension].store.getFreeCapacity(RESOURCE_ENERGY));
+        if(creep.carry.energy < creep.carryCapacity) {
+            // TODO: sources already searched in main
+            // - loop found found sources
+            // - assign (empty) creep to source with most energy
+            var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0]);
+            }
         }
-
-        if(targets.length > 0) {
-          if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+        else {
+          // Room.find(type, [opts]) -> more info, see Room api
+          //    - type = One of the FIND_* constants.
+          //    - opts = filter object, function, string
+          //             The result list will be filtered using the Lodash.filter method.
+          //           In this case:
+          //             - filter: callback funtion
+          //             - callback function as ES6 arrow function: (structure) => { return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0; }
+          //             - what is after 'return' is the "if" condition or structure has to apply to
+          let targets = creep.room.find(FIND_MY_STRUCTURES, { filter: (structure) => { return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0; } });
+          //console.log(targets);
+          for (let target in targets) {
+            console.log(targets[target].structureType + targets[target].id + ' free capacity: ' + targets[target].store.getFreeCapacity(RESOURCE_ENERGY));
+          }
+          if(targets.length > 0) {
+            if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+              creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+            }
           }
         }
-      }
     }
 };
 
