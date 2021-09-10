41a5b00bde33f9ef53582aa3e87509b97678bd6f
Stijn Mertens <stijn.mertens@expertum.net>
2021-09-10 21:41:19 +0200

    - Prettify syntax - Added TODO ideas

--- src/role.builder.js
+++ src/role.builder.js
@@ -1,32 +1,44 @@
 var roleBuilder = {
 
-    /** @param {Creep} creep **/
-    run: function(creep) {
+  /** @param {Creep} creep **/
+  run: function (creep) {
+    /*
+    // TODO:
+    - harvest -> when empty, first fill
+    - build -> first do building tasks
+    - repair -> next do repair tasks
+      -> remember repair task -> until completely done, keep doing this one!
+      -> Repair task prio?
+          - Spawn
+          - Tower
+          - Extensions
+          - Road
+    */
+    // 🛠️
 
-            if(creep.memory.building && creep.carry.energy == 0) {
-            creep.memory.building = false;
-            creep.say('🔄 harvest');
-            }
-            if(!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
-                creep.memory.building = true;
-                creep.say('🚧 build');
-            }
+    if (creep.memory.building && creep.carry.energy == 0) {
+      creep.memory.building = false;
+      creep.say('🔄 harvest');
+    }
+    if (!creep.memory.building && creep.carry.energy == creep.carryCapacity) {
+      creep.memory.building = true;
+      creep.say('🚧 build');
+    }
 
-            if(creep.memory.building) {
-                const target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
-                if(target) {
-                  if(creep.build(target) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
-                  }
-                }
-            }
-            else {
-                var sources = creep.room.find(FIND_SOURCES);
-                if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                  creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-                }
-            }
+    if (creep.memory.building) {
+      const target = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+      if (target) {
+        if (creep.build(target) == ERR_NOT_IN_RANGE) {
+          creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
         }
+      }
+    } else {
+      var sources = creep.room.find(FIND_SOURCES);
+      if (creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+        creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+      }
+    }
+  }
 };
 
 module.exports = roleBuilder;
